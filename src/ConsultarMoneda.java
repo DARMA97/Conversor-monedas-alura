a238810dddb687b02040474f0551fad8c1b447d6
Diego Arbelaez <difer2511@gmail.com>
2024-12-10 18:35:50 -0500

    Se Crear archivo para consultar moneda con la API Se agrega funcion para consultar moneda individual

--- src/ConsultarMoneda.java
+++ src/ConsultarMoneda.java
@@ -6,8 +6,9 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsultarMoneda {
-    public Monedas consultaMoneda (String monedaBase, String monedaTarget){
-        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/741ddd6ca623bf32d3894f31/"+ monedaBase + "/" + monedaTarget);
+    public Monedas consultaMoneda (String monedaBase, String monedaTarget, String amount){
+        double cantidad = Double.parseDouble(amount);
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/741ddd6ca623bf32d3894f31/pair/"+ monedaBase + "/" + monedaTarget + "/" + cantidad);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(direccion)
@@ -19,6 +20,24 @@ public class ConsultarMoneda {
 
         } catch (Exception e) {
             throw new RuntimeException("No se encontro la moneda consultada");
+
+        }
+
+    }
+    public Monedas consultaIndividual (String monedaBase){
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/741ddd6ca623bf32d3894f31/latest/"+ monedaBase);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return  new Gson().fromJson(response.body(), Monedas.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException("No se encontro la moneda consultada");
+
         }
     }
 }
